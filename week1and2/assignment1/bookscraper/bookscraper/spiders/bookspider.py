26d90a8907baccda4954e9bf32d610a00c9d2d01
carrliitos <salazar.ten80@gmail.com>
2024-02-04 07:01:00 -0600

    Added docstring and start process to extract more book information

--- week1and2/assignment1/bookscraper/bookscraper/spiders/bookspider.py
+++ week1and2/assignment1/bookscraper/bookscraper/spiders/bookspider.py
@@ -7,20 +7,39 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
+    """
+    Parses the response from the main page of the 'books.toscrape.com' website.
+
+    Extracts information about books from the provided HTML response using CSS selectors.
+    For each book, follows the link to its detailed page and invokes the 'parse_book_page'
+    callback to extract additional information.
+
+    Args:
+        response (scrapy.http.Response): The HTTP response object containing the HTML content.
+
+    Yields:
+        scrapy.Request: A scrapy Request object to follow links to individual book pages.
+
+    Returns:
+        None
+    """
         books = response.css("article.product_pod")
 
         for book in books:
-            yield {
-                "name": book.css("h3 a::text").get(),
-                "price": book.css(".product_price .price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"]
-            }
+            relative_url = book.css("h3 a ::attr(href)").get()
 
-        next_page = response.css("li.next a ::attr(href)").get()
+            if "catalogue/" in relative_url:
+                next_page_url = "https://books.toscrape.com/" + relative_url
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + relative_url
+            yield response.follow(next_page_url, callback=self.parse_book_page)
 
         if next_page is not None:
             if "catalogue/" in next_page:
-                next_page_url = "https://books.toscrape.com/" + next_page
+                boook_url = "https://books.toscrape.com/" + next_page
             else:
-                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
-            yield response.follow(next_page_url, callback=self.parse)
+                boook_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(boook_url, callback=self.parse)
+
+    def parse_book_page(self, rsponse):
+        pass
